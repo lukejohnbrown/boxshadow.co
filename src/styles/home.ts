bf4b5e8eda5bcf835b37a41531629d68c9feaa4f
lukejohnbrown@gmail.com <lukejohnbrown@gmail.com>
2020-01-17 05:49:54 +0000

    Fix build by changing dir structure of pages

--- src/styles/home.ts
+++ src/styles/home.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import theme from "../../theme";
+import theme from "../theme";
 
 export const AboutBlockWrapper = styled.div`
   @media ${theme.breakpoints.up.lg} {
