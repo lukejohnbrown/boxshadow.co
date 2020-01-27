152e3e83901f95e00456c26dc9a4901686beb776
lukejohnbrown@gmail.com <lukejohnbrown@gmail.com>
2020-01-27 06:20:38 +0000

    Stupid syntax error, my bad

--- src/html.js
+++ src/html.js
@@ -8,25 +8,20 @@ export default function HTML(props) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                  <!-- Fathom - simple website analytics - https://usefathom.com -->
-                  <
-                  script >
-                    (function (f, a, t, h, o, m) {
-                      a[h] = a[h] || function () {
-                        (a[h].q = a[h].q || []).push(arguments)
-                      };
-                      o = f.createElement('script'),
-                        m = f.getElementsByTagName('script')[0];
-                      o.async = 1;
-                      o.src = t;
-                      o.id = 'fathom-script';
-                      m.parentNode.insertBefore(o, m)
-                    })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
-                  fathom('set', 'siteId', 'LMFXEHMY');
-                  fathom('trackPageview'); <
-                  /script> <
-                  !--/ Fathom -->
-                `,
+                (function (f, a, t, h, o, m) {
+                  a[h] = a[h] || function () {
+                    (a[h].q = a[h].q || []).push(arguments)
+                  };
+                  o = f.createElement('script'),
+                    m = f.getElementsByTagName('script')[0];
+                  o.async = 1;
+                  o.src = t;
+                  o.id = 'fathom-script';
+                  m.parentNode.insertBefore(o, m)
+                })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+              fathom('set', 'siteId', 'LMFXEHMY');
+              fathom('trackPageview');
+            `,
           }}
         />
         <meta charSet="utf-8" />
